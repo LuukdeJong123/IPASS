82b1f7c99568a7e33f4ac946ce71e262846bdbd7
Luukdejong123 <luuk.l.dejong@student.hu.nl>
2018-06-25 15:41:41 +0200

    Heroke werk pls

--- src/main/java/nl/hu/ipass/app/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/app/persistence/PostgresBaseDao.java
@@ -9,7 +9,7 @@ public class PostgresBaseDao {
 	private static Connection conn;
 	  protected final Connection getConnection() throws SQLException, ClassNotFoundException {
 		  Class.forName("org.postgresql.Driver");
-		  conn = DriverManager.getConnection("jdbc:postgresql://localhost/IPASS?user=postgres&password=admin");
+		  conn = DriverManager.getConnection("jdbc:postgresql://ec2-54-247-87-201.eu-west-1.compute.amazonaws.com/d7aaef90cv9mbg?user=todrtwfcbsyfmj&password=5046b4f0d816f652d98139b3fd29dd3605ac3e991ba6b802e924209c5408d659");
 		  //Dit zijn de beide connecties die ik leg met de database voor heroku en mijn eigen localhost
 		  //jdbc:postgresql://localhost/IPASS?user=postgres&password=admin
 		  //jdbc:postgresql://ec2-54-247-87-201.eu-west-1.compute.amazonaws.com/d7aaef90cv9mbg?user=todrtwfcbsyfmj&password=5046b4f0d816f652d98139b3fd29dd3605ac3e991ba6b802e924209c5408d659
